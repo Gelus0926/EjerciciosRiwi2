f940e4ae6460a90d272cc7b6168a4747cc46559b
Gelus0926 <mateofrako@gmail.com>
2023-10-04 19:55:33 -0500

    Ejercicios de logica en python propuesto en la clase de desarrollo

---
+++ 3.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite un número: "))
+
+Cuadrado = numero ** 2
+
+print("El cuadrado de el número digitado es: ", Cuadrado)
---
+++ 4.py
@@ -0,0 +1,11 @@
+n1 = int(input("Ingrese el primer número: "))
+n2 = int(input("Ingrese el segundo número: "))
+n3 = int(input("Ingrese el tercero número: "))
+n4 = int(input("Ingrese el cuarto número: "))
+n5 = int(input("Ingrese el quinto número: "))
+n6 = int(input("Ingrese el sexto número: "))
+n7 = int(input("Ingrese el septimo número: "))
+
+promedio = n1+n2+n3+n4+n5+n6+n7 / 7
+
+print("El promedio de los siete números es: ", promedio)
